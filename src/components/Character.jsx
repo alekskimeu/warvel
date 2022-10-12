f6294e3ad5478684f690ae631738374033db55ba
alekskimeu <kim3ual3x@gmail.com>
2022-10-12 21:36:22 +0300

    Added logo & favicons

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -29,12 +29,12 @@ const Character = ({ character }) => {
 					/>
 				</div>
 				<div className="absolute top-0 left-0 right-0 bottom-0 h-full w-full opacity-0 group-hover:opacity-100 rounded-lg transition ease-in-out delay-150 duration-500 character-info">
-					<div className="absolute text-center details">
+					<div className="absolute text-center details w-full">
 						<h1 className="text-xl font-semibold mb-2 text-gray-400 transition ease-in-out delay-150 duration-500">
 							{character.name}
 						</h1>
 						<button
-							className="mt-2 bg-[#ec1d24] text-white font-semibold px-4 py-2 rounded-md btn"
+							className="mt-2 bg-[#ec1d24] text-white font-semibold px-5 py-2 rounded-md btn"
 							onClick={() => setShow(true)}
 						>
 							View
